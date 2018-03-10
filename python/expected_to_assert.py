f67668c0b2e7602eec62499924ffba44e0561405
Daniel Gunn <dangunn@qq.com>
2018-03-10 13:26:35 +0800

    Adding Warmup2 problem set solutions

--- python/expected_to_assert.py
+++ python/expected_to_assert.py
@@ -2,7 +2,7 @@
 # gives you once youve solved a problem, into a series of
 # assert statements in python
 
-f = open("expected_input.dat","r")
+f = open("expected_input_warmup2.dat","r")
 for line in f:
     print("assert",line.split("\t")[0].replace("→","=="))
 f.close()
